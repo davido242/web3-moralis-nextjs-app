0277d6a757a11c1757600f105527c2662cda12ea
Monday_David_Sarka <david_md@hamari.agency>
2022-06-15 16:54:31 +0100

    added authentication for logout

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import Login from '../components/Login'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  return (
-    <div>
+  const {isAuthenticated, logout} = useMoralis();
+  return <div>{isAuthenticated ? (
+    <p>
+      You are logged in
+      <button onClick= {logout}>Sign Out</button>
+    </p>
+      ):(
       <Login/>
-    </div>
-  )
+    )}
+    
+    </div>;
 }
