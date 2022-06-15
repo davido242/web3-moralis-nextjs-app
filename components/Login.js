10b0dc8b977b92223958c60104d4fb3522cf6d00
Monday_David_Sarka <david_md@hamari.agency>
2022-06-15 14:21:40 +0100

    imported added authentication to components

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,24 @@
 import styles from '../styles/Login.module.css';
 import Image from 'next/image';
 import icon from './Images/logo.svg';
+import { useMoralis } from "react-moralis";
 
 
 function Login () {
+    const {authenticate, authError} = useMoralis();
+
     return (
         <div className={styles.login_container}>
             <div className={styles.login_card}>
                 <Image src={icon} width={100} height={100} />
                 <div className={styles.sign_in_container}>
-                    <button>Login with Moralis</button>
+                    {authError && (
+                        <p className={styles.error}>
+                        {authError.name}
+                        {authError.message}
+                        </p>
+                    )}
+                    <button onClick={authenticate}>Login with Moralis</button>
                 </div>
             </div>
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { MoralisProvider } from "react-moralis";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+    appId='VRaxLh94NmfSKGnj2xXCprXSAr38ojfHLg3wA5h8' 
+    serverUrl='https://z7zog7feyjxe.usemoralis.com:2053/server'>
+        {
+      <Component {...pageProps} />
+        }
+    </MoralisProvider>
+  );
 }
 
 export default MyApp
